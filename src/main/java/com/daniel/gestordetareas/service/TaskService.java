dbfb8702e2607d012eba18aacc3a58099058ca16
Danieel07 <dnietollanos@gmail.com>
2025-08-04 21:50:49 -0500

    Create Service

---
+++ src/main/java/com/daniel/gestordetareas/service/TaskService.java
@@ -0,0 +1,32 @@
+package com.daniel.gestordetareas.service;
+
+import com.daniel.gestordetareas.model.Task;
+import com.daniel.gestordetareas.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+    public Optional<Task> getTaskById(long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(long id) {
+        taskRepository.deleteById(id);
+    }
+
+}
